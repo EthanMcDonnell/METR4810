9a3a141a9e1b67ea9c4138e768f5b25a031eebf6
Ethan McDonnell <ethan@xelle.com>
2023-05-02 21:57:47 +1000

    Changed sdk config and main with new s3 UART

--- main/main.c
+++ main/main.c
@@ -20,18 +20,18 @@
 void app_main(void)
 {
     printf("Hello World it's Ethan\n");
-    init_monitor();
+    //init_monitor(); //DONT USE
     init_pwm();
     xTaskCreate(led_task, "LED Task", 4096, NULL, 10, NULL); // Starts new LED FreeRTOS task
     RobotCommand robot_command = Still;
     while (1)
     {
         //char inputc = read_from_monitor(); // Pauses until reads char
-        char inputc = 'w';
+        //char inputc = 'w';
         
         robot_command = input_handling(inputc); // Finds robot command from given char
         //set_motors(robot_command);              
         printf("%s\n", robot_command_strings[(int)robot_command]);// Print current command to terminal
-        vTaskDelay(1000 / portTICK_PERIOD_MS);                    //FreeRTOS delay
+        vTaskDelay(40 / portTICK_PERIOD_MS);                    //FreeRTOS delay
     }
 }
